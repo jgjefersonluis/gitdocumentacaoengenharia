6a8152621d9e354cf083b248999d1bed2dff5e3f
jgjefersonluis <jgjefersonluis@gmail.com>
2022-05-19 17:04:14 -0300

    books -> models

--- projetolibrary/books/models.py
+++ projetolibrary/books/models.py
@@ -1,3 +1,13 @@
+from uuid import uuid4
 from django.db import models
 
 # Create your models here.
+class Books(models.Model):
+    id_book = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    release_year = models.IntegerField()
+    state = models.CharField(max_length=50)
+    pages = models.IntegerField()
+    publishing_campany = models.CharField(max_length=255)
+    create_at = models.DateField(auto_now_add=True)
